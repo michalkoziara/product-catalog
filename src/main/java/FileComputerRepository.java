fb027ca356977063936986a8e367c28f86ee7a8c
michalkoziara <mkoziara12@gmail.com>
2021-04-19 21:41:18 +0200

    Refactor file repository

--- src/main/java/FileComputerRepository.java
+++ src/main/java/FileComputerRepository.java
@@ -14,22 +14,23 @@ public class FileComputerRepository implements ComputerRepository {
                 String[] data = catalogLine.split(";", -1);
 
                 if (data.length == 16) {
-                    ComputerBuilder computerBuilder = new ComputerBuilder();
-                    computerBuilder.setManufacturerName(data[0]);
-                    computerBuilder.setDiagonalScreenSize(data[1]);
-                    computerBuilder.setScreenResolution(data[2]);
-                    computerBuilder.setScreenSurfaceType(data[3]);
-                    computerBuilder.setTouchscreenFlag(data[4]);
-                    computerBuilder.setCpu(data[5]);
-                    computerBuilder.setNumberOfCpuCores(data[6]);
-                    computerBuilder.setClockFrequency(data[7]);
-                    computerBuilder.setRam(data[8]);
-                    computerBuilder.setDiscSize(data[9]);
-                    computerBuilder.setDiscType(data[10]);
-                    computerBuilder.setGpu(data[11]);
-                    computerBuilder.setGpuMemory(data[12]);
-                    computerBuilder.setOperatingSystem(data[13]);
-                    computerBuilder.setPhysicalDriveType(data[14]);
+                    ComputerBuilder computerBuilder =
+                            new ComputerBuilder()
+                                    .setManufacturerName(data[0])
+                                    .setDiagonalScreenSize(data[1])
+                                    .setScreenResolution(data[2])
+                                    .setScreenSurfaceType(data[3])
+                                    .setTouchscreenFlag(data[4])
+                                    .setCpu(data[5])
+                                    .setNumberOfCpuCores(data[6])
+                                    .setClockFrequency(data[7])
+                                    .setRam(data[8])
+                                    .setDiscSize(data[9])
+                                    .setDiscType(data[10])
+                                    .setGpu(data[11])
+                                    .setGpuMemory(data[12])
+                                    .setOperatingSystem(data[13])
+                                    .setPhysicalDriveType(data[14]);
 
                     computers.add(computerBuilder.createComputer());
                 }
